48ccb891fc3103686e1e65e60701087152067a13
prashantbhoite123 <pbhoite985@gmail.com>
2024-05-05 04:28:09 +0530

    mainner changes

---
+++ script.js
@@ -0,0 +1,18 @@
+const hour = document.querySelector("#hour");
+const menuts = document.querySelector("#menuts");
+const second = document.querySelector("#second");
+
+setInterval(() => {
+  let d = new Date();
+  htime = d.getHours();
+  mtime = d.getMinutes();
+  stime = d.getSeconds();
+
+  hrotation = 30 * htime + mtime / 2;
+  mrotation = 6 * mtime;
+  srotation = 6 * stime;
+
+  hour.style.transform = `rotate(${hrotation}deg)`;
+  menuts.style.transform = `rotate(${mrotation}deg)`;
+  second.style.transform = `rotate(${srotation}deg)`;
+}, 1000);
